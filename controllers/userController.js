460ff82a76e1d2d94b98c12adab1ae914e12c8e6
Prakhar <Prakhar.agra10@gmail.com>
2025-03-01 00:50:30 +0530

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ const createToken = (_id) => {
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL,
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
